ccd706d24ca53c51a826770826b50b84e91988cf
Luis Pinillos <pinillosluisfer@gmail.com>
2026-07-29 17:45:31 -0500

    chore: few exercises completed - 1.3 Numbers

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce_back = 3/5
+
+for i in range(1,11):
+    height = height * bounce_back
+    print(i,round(height, 4))
+    
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,31 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+months = 0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while principal > 0:
+    months += 1
+    if extra_payment_start_month <= months <= extra_payment_end_month:
+        pay = payment + extra_payment
+    else:
+        pay = payment
+
+    balance = principal * (1+rate/12)
+
+    if pay > balance:
+        pay = balance
+
+    principal = balance - pay
+    total_paid += pay
+    print(months, round(total_paid, 4), round(principal, 4))
+            
+
+print('Total paid', round(total_paid, 4))
+print("Months:", months)
