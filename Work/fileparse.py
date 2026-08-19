1514109ec8c7696f16ceee62b55077e9abf3b1b9
Luis Pinillos <pinillosluisfer@gmail.com>
2026-08-19 13:57:59 -0500

    hotfix: minor logical fixes

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -2,38 +2,34 @@
 #
 # Exercise 3.3
 import csv
-def parse_csv(filename, select=None, types=None, has_headers=False, delimiter=',') -> dict:
+def parse_csv(filename, select=None, types=None, has_headers=True, delimiter=',') -> list:
     '''
     Parse a CSV file into a list of records
     '''
     with open(filename) as f:
         rows = csv.reader(f,delimiter=delimiter)
         records = []
-        if has_headers:
-            headers = next(rows)
+        headers = next(rows)
 
-            if select:
-                indices = [headers.index(colname) for colname in select]
-                headers = select
-            else:
-                indices = []
+        if select:
+            indices = [headers.index(colname) for colname in select]
+            headers = select
+        else:
+            indices = []
 
-            for row in rows:
-                if not row:
-                    continue
-                if indices:
-                    row = [row[index] for index in indices]
-                if types:
-                    row = [func(val) for func, val in zip(types,row)]
+        for row in rows:
+            if not row:
+                continue
+            if indices:
+                row = [row[index] for index in indices]
+            if types:
+                row = [func(val) for func, val in zip(types,row)]
+            if has_headers:
                 record = dict(zip(headers,row))
                 records.append(record)
-        else:
-            for row in rows:
-                if not row:
-                    continue
-                if types:
-                    row = [func(val) for func, val in zip(types, row)]
+            else:
                 record = tuple(row)
                 records.append(record)
+                
         return records
 
